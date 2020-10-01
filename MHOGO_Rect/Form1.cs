51263475fe0fb12132946736dee65b9b96b75c3b
Vadim <55794804+Vadimsdl@users.noreply.github.com>
2020-10-01 11:23:50 +0300

    Vadimsdl

---
+++ MHOGO_Rect/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MHOGO_Rect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            labArr = new List<Label>();
+            mouslast = new List<Point>();
+   
+        }
+
+        Rect R = new Rect();
+        int W = 0, lastX = 0, lastY = 0;
+        Point lastP;
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            if (e.Button == MouseButtons.Right)
+            {
+                R.r = new Rectangle(e.X, e.Y, 150, 150);
+                R.draw(g);
+                StandartNW = R.r.Width;
+            }
+
+            if (e.Button == MouseButtons.Left && R.r != null && W == R.r.Width && lastX == R.r.X && lastY == R.r.Y)
+            {
+                if ((R.r.X <= e.X && R.r.Width + R.r.X >= e.X) && (R.r.Y <= e.Y && R.r.Height + R.r.Y >= e.Y) && mous.X != -1 && mous.Y != -1)
+                {
+
+                    if (R.r.X <= e.X && R.r.Y <= e.Y && (R.r.Width / 3) + R.r.X > e.X && ((R.r.Height / 3) + (R.r.Y - 10)) > e.Y)
+                        KUBinKUB = 1;
+                    else if ((R.r.Width / 3) + R.r.X <= e.X && R.r.Y <= e.Y && (R.r.Height / 3) + (R.r.Y - 10) > e.Y && (R.r.Width / 3) * 2 + R.r.X > e.X)
+                        KUBinKUB = 2;
+                    else if (R.r.Y <= e.Y && (R.r.Width / 3) * 2 + R.r.X <= e.X && R.r.X + R.r.Width > e.X && ((R.r.Height / 3) + (R.r.Y - 10)) > e.Y)
+                        KUBinKUB = 3;
+                    else if (R.r.X <= e.X && ((R.r.Height / 3) + R.r.Y - 10) <= e.Y && R.r.X + R.r.Width > e.X && ((R.r.Height / 3) * 2 + R.r.Y + 10) > e.Y)
+                        KUBinKUB = 4;
+                    else if (((R.r.Height / 3) * 2 + R.r.Y - 10) <= e.Y && R.r.X <= e.X && R.r.Y + R.r.Height > e.Y && ((R.r.Width / 3) + R.r.X) > e.X)
+                        KUBinKUB = 5;
+                    else if ((R.r.Width / 3) + R.r.X <= e.X && R.r.Y + 2 * (R.r.Height / 3) + 10 <= e.Y && (R.r.Width / 3) * 2 + R.r.X > e.X && R.r.Height + R.r.Y > e.Y)
+                        KUBinKUB = 6;
+                    else if ((R.r.Width / 3) * 2 + R.r.X <= e.X && R.r.Y + 2 * (R.r.Height / 3) + 10 <= e.Y && R.r.Y + R.r.Height > e.Y && R.r.X + R.r.Width > e.X)
+                        KUBinKUB = 7;
+                    else
+                        KUBinKUB = 0;
+
+                    TextBox textBox = new TextBox();
+                    textBox.AutoSize = true;
+                    textBox.Location = new System.Drawing.Point(R.r.X, R.r.Y - 19);
+                    textBox.Size = new System.Drawing.Size(100, 150);
+                    textBox.KeyUp += textBox_KeyPress;
+
+                    Controls.Add(textBox);
+                    lastP = new Point(e.X, e.Y);
+                    mouslast.Add(new Point(e.X - R.r.X, e.Y - R.r.Y));
+                    toolStripStatusLabel1.Text = KUBinKUB.ToString();
+                }
+            }
+            else
+            {
+                W = R.r.Width;
+                lastX = R.r.X;
+                lastY = R.r.Y;
+            }
+            R.draw(g);
+        }
+        List<Label> labArr;
+        int KUBinKUB = 0;
+        private void textBox_KeyPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                var Linklab = (TextBox)sender;
+
+                if (Linklab != null)
+                {
+                    Label label = new Label();
+                    label.AutoSize = true;
+
+                    KubinKub(label, KUBinKUB);
+
+                    label.Size = new System.Drawing.Size(35, 13);
+                    label.Text = Linklab.Text;
+                    labArr.Add(label);
+
+                    Controls.Add(labArr.Last());
+
+                    Controls.Remove(Linklab);
+
+                }
+                R.draw(CreateGraphics());
+            }
+
+
+        }
+
+        void KubinKub(Label label, int KIK)
+        {
+            switch (KIK)
+            {
+                case 1:
+                    label.Location = lab = new System.Drawing.Point(lastP.X, lastY);
+                    break;
+                case 2:
+                    label.Location = lab = new System.Drawing.Point(lastP.X, lastY);
+                    break;
+                case 3:
+                    label.Location = lab = new System.Drawing.Point(lastP.X, lastY);
+                    break;
+                case 4:
+                    label.Location = lab = new System.Drawing.Point(lastP.X, lastY);
+                    break;
+                case 5:
+                    label.Location = lab = new System.Drawing.Point(lastP.X, lastY);
+                    break;
+                case 6:
+                    label.Location = lab = new System.Drawing.Point(lastP.X, lastY);
+                    break;
+                case 7:
+                    label.Location = lab = new System.Drawing.Point(lastP.X, lastY);
+                    break;
+                default:
+                    MessageBox.Show("Какая-то ошибка");
+                    break;
+            }
+
+        }
+
+        void ResizeRect(Point pt)
+        {
+            if (startResize)
+            {
+                switch (R.strongInBorder(pt))
+                {
+                    case PosType.West:
+                        R.r.Width = -pt.X + R.r.X + R.r.Width;
+                        R.r.X = pt.X;
+                        break;
+                    case PosType.East:
+                        R.r.Width = pt.X - R.r.X;
+                        break;
+                }
+
+            }
+        }
+
+
+        bool startResize = false;
+        Point cursorPoint;
+        bool isSelected = false;
+        Point mous;
+        List<Point> mouslast;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (R.r.X <= e.X && R.r.Width + R.r.X >= e.X && R.r.Y <= e.Y && R.r.Height + R.r.Y >= e.Y)
+                mous = new Point(e.X - R.r.X, e.Y - R.r.Y);
+            else mous = new Point(-1, -1);
+
+            if (!isSelected && R.into(e.Location) && e.Button == MouseButtons.Left)
+            {
+                R.Selected = isSelected = true;
+
+            }
+            if (!R.into(e.Location) && e.Button == MouseButtons.Left)
+            {
+                R.Selected = isSelected = false;
+
+
+            }
+            if (e.Button == MouseButtons.Left && R.inBorder(e.Location) != PosType.None)
+            {
+                startResize = true;
+                cursorPoint = e.Location;
+
+                switch (R.inBorder(e.Location))
+                {
+                    case PosType.WE:
+                        Cursor = Cursors.SizeWE;
+                        break;
+                    case PosType.None:
+                        Cursor = Cursors.Default;
+                        break;
+                }
+            }
+
+        }
+        int nX = 0, nY = 0, nW = 0;
+
+        private void label_LocationChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        Point lab;
+        int StandartNW = 0, resultNW = 0;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Left && startResize)
+            {
+                //Invalidate();  (R.r.Width / 3)
+                ResizeRect(e.Location);
+                R.draw(CreateGraphics());
+                resultNW = R.r.Width - StandartNW;
+                if (nW < (R.r.Width - 10) || nW > (R.r.Width + 10))
+                {
+                    Invalidate();
+                    nW = R.r.Width;
+
+                }
+            }
+            else
+            if (e.Button == MouseButtons.Left)
+            {
+
+                if (R.r.X <= e.X && R.r.Width + R.r.X >= e.X && R.r.Y <= e.Y && R.r.Height + R.r.Y >= e.Y && mous.X != -1 && mous.Y != -1)
+                {
+                    R.Loc(mous, e.Location);
+                    R.draw(CreateGraphics());
+                    if (nX < (R.r.X - 10) || nY < (R.r.Y - 10) || nY > (R.r.Y + 10) || nX > (R.r.X + 10))
+                    {
+                        Invalidate();
+                        nX = R.r.X;
+                        nY = R.r.Y;
+                    }
+                    try
+                    {
+
+                        // labArr.Last().Left = R.r.X + mouslast.X;
+                        // labArr.Last().Top = R.r.Y + mouslast.Y;
+                        int ind = 0;
+                        foreach (Label l in labArr)
+                        {
+                            l.Left= R.r.X + mouslast[ind].X ;
+                            l.Top = R.r.Y + mouslast[ind].Y;
+                            ind++;
+                        }
+
+                    }
+                    catch { }
+                }
+            }
+
+            switch (R.inBorder(e.Location))
+            {
+                case PosType.WE:
+                    Cursor = Cursors.SizeWE;
+                    break;
+                case PosType.None:
+                    Cursor = Cursors.Default;
+                    break;
+            }
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            startResize = false;
+            cursorPoint = e.Location;
+            StandartNW = R.r.Width;
+        }
+    }
+}
---
+++ MHOGO_Rect/Rect.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MHOGO_Rect
+{
+    enum PosType { None, West, East, WE };
+
+    abstract class Shapes
+    {
+
+        public Rectangle r;
+        public bool Selected { get; set; } = false;
+        int _5 = 5;
+        public bool into(Point p)
+        {
+            return (p.X > r.X && p.X < r.X + r.Width && p.Y > r.Y && p.Y < r.Y + r.Height);
+        }
+        public abstract void draw(Graphics g);
+        public virtual PosType inBorder(Point p)
+        {
+            _5 = 5;
+            if (near(p.X, r.X) || near(p.X, r.X + r.Width))
+            {
+                return PosType.WE;
+            }
+            else
+                return PosType.None;
+        }
+        public virtual PosType strongInBorder(Point p)
+        {
+            _5 = 10;
+            if (near(p.X, r.X))
+            {
+                return PosType.West;
+            }
+            if (near(p.X, r.X + r.Width))
+            {
+                return PosType.East;
+            }
+            else
+                return PosType.None;
+        }
+
+        public virtual void Loc(Point p, Point e)
+        {
+            r.X = e.X - p.X;
+            r.Y = e.Y - p.Y;
+        }
+
+        bool near(int x, int y)
+        {
+            if (x > y - _5 && x < y + _5)
+                return true;
+            else
+                return false;
+        }
+
+    }
+    class Rect : Shapes
+    {
+        public override void draw(Graphics g)
+        {
+            if (Selected)
+            {
+
+                Rectangle r2d2 = new Rectangle(r.X + 2, r.Y + 2, r.Width - 4, r.Height - 4);
+                g.FillRectangle(Brushes.Gray, r2d2);
+                Pen pen = new Pen(Color.Black, 1);
+                pen.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+                g.DrawRectangle(pen, r);
+                g.DrawLine(Pens.Black, (r2d2.Width / 3) + r2d2.X, r2d2.Y, (r2d2.Width / 3) + r2d2.X, (r2d2.Height / 3) + r2d2.Y-10);
+                g.DrawLine(Pens.Black, (r2d2.Width / 3) * 2 + r2d2.X, r2d2.Y, (r2d2.Width / 3) * 2 + r2d2.X, (r2d2.Height / 3) + r2d2.Y-10);
+                g.DrawLine(Pens.Black, r2d2.X, (r2d2.Height / 3) + r2d2.Y-10, r2d2.X + r2d2.Width, (r2d2.Height / 3) + r2d2.Y-10);
+                g.DrawLine(Pens.Black, r2d2.X, (r2d2.Height / 3) * 2 + r2d2.Y + 10, r2d2.X + r2d2.Width, (r2d2.Height / 3) * 2 + r2d2.Y + 10);
+                g.DrawLine(Pens.Black, (r2d2.Width / 3) + r2d2.X, r2d2.Height + r2d2.Y, (r2d2.Width / 3) + r2d2.X, r2d2.Y + 2 * (r2d2.Height / 3)+10);
+                g.DrawLine(Pens.Black, (r2d2.Width / 3) * 2 + r2d2.X, r2d2.Height + r2d2.Y, (r2d2.Width / 3) * 2 + r2d2.X, r2d2.Y + 2 * (r2d2.Height / 3)+10);
+            }
+            else
+            {
+
+
+                g.FillRectangle(Brushes.Gray, r);
+                g.DrawRectangle(Pens.Black, r);
+                g.DrawLine(Pens.Black, (r.Width / 3) + r.X, r.Y, (r.Width / 3) + r.X, (r.Height / 3) + r.Y-10);
+                g.DrawLine(Pens.Black, (r.Width / 3) * 2 + r.X, r.Y, (r.Width / 3) * 2 + r.X, (r.Height / 3) + r.Y-10);
+                g.DrawLine(Pens.Black, r.X, (r.Height / 3) + r.Y-10, r.X + r.Width, (r.Height / 3) + r.Y-10);
+                g.DrawLine(Pens.Black, r.X, (r.Height / 3) * 2 + r.Y + 10, r.X + r.Width, (r.Height / 3) * 2 + r.Y + 10);
+                g.DrawLine(Pens.Black, (r.Width / 3) + r.X, r.Height + r.Y, (r.Width / 3) + r.X, r.Y + 2 * (r.Height / 3)+10);
+                g.DrawLine(Pens.Black, (r.Width / 3) * 2 + r.X, r.Height + r.Y, (r.Width / 3) * 2 + r.X, r.Y + 2 * (r.Height / 3)+10);
+            }
+        }
+
+
+
+    }
+}
